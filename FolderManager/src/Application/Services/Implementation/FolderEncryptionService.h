5958480862bc5158c4a8676e4ef7dc60d4664467
kapotapov <kapotapov@viarune.com>
2026-05-05 05:20:52 +0700

    add description of application to readme, atomic deletion implementation in ecrypt and decrypt method, singleton pattern to FolderEncryptorService

--- FolderManager/src/Application/Services/Implementation/FolderEncryptionService.h
+++ FolderManager/src/Application/Services/Implementation/FolderEncryptionService.h
@@ -7,7 +7,7 @@ using namespace std;
 namespace Application::Services::Implementation {
     class FolderEncryptionService: public Application::Services::IFolderEnryptionService {
     public:
-        FolderEncryptionService(shared_ptr<Application::InfrastructureServices::ICryptoPPManager> cipher);
+        FolderEncryptionService(const shared_ptr<Application::InfrastructureServices::ICryptoPPManager> cipher);
         size_t encryptFiles(const filesystem::path& folderPath) override;
         size_t decryptFiles(const filesystem::path& folderPath) override;
 
